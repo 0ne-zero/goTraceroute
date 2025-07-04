2c0529b0631a5cb8c08af3ba638fcd1cadb3999e
0ne-zero <pouria.khakpour9909@gmail.com>
2025-07-04 17:20:58 +0000

    Chg: implement better cli tool with extensive flags

--- cmd/gotraceroute.go
+++ cmd/gotraceroute.go
@@ -1,59 +1,94 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/0ne-zero/traceroute"
+	"github.com/0ne-zero/traceroute/cmd/flag"
+	trace_socket "github.com/0ne-zero/traceroute/net/socket"
 )
 
-func printHop(hop traceroute.TracerouteHop) {
-	addr := fmt.Sprintf("%v.%v.%v.%v", hop.Address[0], hop.Address[1], hop.Address[2], hop.Address[3])
-	hostOrAddr := addr
-	if hop.Host != "" {
-		hostOrAddr = hop.Host
-	}
-	if hop.Success {
-		fmt.Printf("%-3d %v (%v)  %v\n", hop.TTL, hostOrAddr, addr, hop.ElapsedTime)
-	} else {
-		fmt.Printf("%-3d *\n", hop.TTL)
-	}
-}
-
 func main() {
-	var m = flag.Int("m", traceroute.DefaultMaxHops, `Set the max time-to-live (max number of hops) used in outgoing probe packets (default is 64)`)
-	var f = flag.Int("f", traceroute.DefaultFirstHop, `Set the first used time-to-live, e.g. the first hop (default is 1)`)
-	var q = flag.Int("q", 1, `Set the number of probes per "ttl" (default is one probe).`)
-
-	flag.Parse()
-	host := flag.Arg(0)
-	options := traceroute.NewTracerouteOptions()
-	options.SetRetries(*q - 1)
-	options.SetMaxHops(*m + 1)
-	options.SetFirstHop(*f)
-
-	ipAddr, err := net.ResolveIPAddr("ip", host)
+	flags, err := flag.ParseFlags()
 	if err != nil {
-		return
+		if errors.Is(err, flag.ErrMissingHost) {
+			flag.PrintUsage()
+			os.Exit(1)
+		}
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("traceroute to %v (%v), %v hops max, %v byte packets\n", host, ipAddr, options.MaxHops(), options.PacketSize())
+	// Resolve host
+	ipAddr, err := net.ResolveIPAddr("ip", flags.Host)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to resolve host %s: %v\n", flags.Host, err)
+		os.Exit(1)
+	}
 
-	c := make(chan traceroute.TracerouteHop)
+	// Setup options
+	opts := traceroute.NewTracerouteOptions()
+	opts.SetMaxHops(flags.MaxHops)
+	opts.SetFirstHop(flags.FirstHop)
+	opts.SetRetries(flags.Retries)
+	opts.SetTimeoutMs(flags.TimeoutMs)
+	opts.SetPacketSize(flags.PacketSize)
+
+	switch flags.PreferAddressFamily {
+	case 4:
+		opts.SetPreferAddressFamily(trace_socket.AF_INET)
+	case 6:
+		opts.SetPreferAddressFamily(trace_socket.AF_INET6)
+	default:
+		opts.SetPreferAddressFamily(trace_socket.AF_INET)
+	}
+
+	fmt.Printf("traceroute to %s (%s), %d hops max, %d byte packets\n",
+		flags.Host, ipAddr.String(), opts.MaxHops(), opts.PacketSize())
+
+	// Setup Ctrl+C handling
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		for {
-			hop, ok := <-c
-			if !ok {
-				fmt.Println()
-				return
-			}
+		<-sigChan
+		fmt.Println("\nInterrupted. Stopping traceroute...")
+		//cancel()
+	}()
+
+	// Start traceroute
+	hopChan := make(chan traceroute.TracerouteHop)
+	go func() {
+		for hop := range hopChan {
 			printHop(hop)
 		}
 	}()
 
-	_, err = traceroute.Traceroute(host, options, c)
+	_, err = traceroute.Traceroute(flags.Host, opts, hopChan)
+	//_, err = traceroute.TracerouteContext(ctx, flags.Host, opts, hopChan)
 	if err != nil {
-		fmt.Printf("Error: %v ", err)
+		fmt.Fprintf(os.Stderr, "Traceroute error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func printHop(hop traceroute.TracerouteHop) {
+	addr := net.IP(hop.Address).String()
+	display := addr
+	if hop.Host != "" {
+		display = hop.Host
+	}
+
+	if hop.Success {
+		fmt.Printf("%-3d %-30s (%-15s)  %6.2f ms\n",
+			hop.TTL, display, addr, float64(hop.ElapsedTime.Microseconds())/1000.0)
+	} else {
+		fmt.Printf("%-3d *\n", hop.TTL)
 	}
 }
