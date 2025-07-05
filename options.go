beb85b3982cd4c384d05af9bb5660efcfdbd7c84
0ne-zero <pouria.khakpour9909@gmail.com>
2025-07-05 15:33:51 +0330

    Impl: destPort & srcPort in traceroute config

--- options.go
+++ options.go
@@ -3,18 +3,20 @@ package traceroute
 import trace_socket "github.com/0ne-zero/traceroute/net/socket"
 
 const (
-	DefaultPort                = 33434
+	DefaultSrcPort             = 33777
+	DefaultDestPort            = 33434
 	DefaultMaxHops             = 64
 	DefaultFirstHop            = 1
 	DefaultTimeoutMs           = 500
 	DefaultRetries             = 3
-	DefaultPacketSize          = 52
+	DefaultPacketSize          = 512
 	DefaultPreferAddressFamily = trace_socket.AF_INET
 )
 
 // tracerouteOptions holds configuration for a traceroute operation.
 type tracerouteOptions struct {
-	port                int
+	destPort            int
+	srcPort             int
 	maxHops             int
 	firstHop            int
 	timeoutMs           int
@@ -26,7 +28,8 @@ type tracerouteOptions struct {
 // NewTracerouteOptions returns TracerouteOptions initialized with defaults.
 func NewTracerouteOptions() *tracerouteOptions {
 	return &tracerouteOptions{
-		port:                DefaultPort,
+		srcPort:             DefaultSrcPort,
+		destPort:            DefaultDestPort,
 		maxHops:             DefaultMaxHops,
 		firstHop:            DefaultFirstHop,
 		timeoutMs:           DefaultTimeoutMs,
@@ -35,8 +38,8 @@ func NewTracerouteOptions() *tracerouteOptions {
 		preferAddressFamily: DefaultPreferAddressFamily,
 	}
 }
-
-func (o *tracerouteOptions) Port() int                { return o.port }
+func (o *tracerouteOptions) SrcPort() int             { return o.srcPort }
+func (o *tracerouteOptions) DestPort() int            { return o.destPort }
 func (o *tracerouteOptions) MaxHops() int             { return o.maxHops }
 func (o *tracerouteOptions) FirstHop() int            { return o.firstHop }
 func (o *tracerouteOptions) TimeoutMs() int           { return o.timeoutMs }
@@ -44,10 +47,10 @@ func (o *tracerouteOptions) MaxRetries() int          { return o.maxRetries }
 func (o *tracerouteOptions) PacketSize() int          { return o.packetSize }
 func (o *tracerouteOptions) PreferAddressFamily() int { return o.preferAddressFamily }
 
-func (o *tracerouteOptions) SetPort(port int)              { o.port = port }
+func (o *tracerouteOptions) SetSrcPort(port int)           { o.srcPort = port }
+func (o *tracerouteOptions) SetDestPort(port int)          { o.destPort = port }
 func (o *tracerouteOptions) SetMaxHops(maxHops int)        { o.maxHops = maxHops }
 func (o *tracerouteOptions) SetFirstHop(firstHop int)      { o.firstHop = firstHop }
 func (o *tracerouteOptions) SetTimeoutMs(timeoutMs int)    { o.timeoutMs = timeoutMs }
 func (o *tracerouteOptions) SetRetries(retries int)        { o.maxRetries = retries }
-func (o *tracerouteOptions) SetPacketSize(size int)        { o.packetSize = size }
 func (o *tracerouteOptions) SetPreferAddressFamily(af int) { o.preferAddressFamily = af }
